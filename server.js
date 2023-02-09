7f757399af6535d8bb2c071ae5cce2e932442b4f
Calvin Chern <calvin.chern24@gmail.com>
2023-02-09 14:24:02 -0500

    reorganizing

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
@@ -33,8 +34,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-app.listen(process.env.PORT || 3001, () => {
-  console.log(`App listening on port ${process.env.PORT || 3001}!`);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
   sequelize.sync({ force: false });
 });
 
