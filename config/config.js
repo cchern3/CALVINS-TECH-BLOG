ff52bdfa749d5ce10f3e57ba585876e8074a4326
Calvin Chern <calvin.chern24@gmail.com>
2023-02-09 21:18:54 -0500

    jawsdb

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// creating a connection for the db
+// creating a connection for the database
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
